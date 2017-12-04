1bd4fd1b0636839fa247747a054b660780169626
Tadas Stasiulionis <tadas.stasiulionis@gmail.com>
2017-12-04 17:21:27 +0200

    Controller and Model created.

---
+++ src/Controller/ArticlesController.php
@@ -0,0 +1,14 @@
+<?php 
+
+namespace App\Controller;
+
+class ArticlesController extends AppController {
+
+    public function index() {
+        $articles = $this->Articles->find('all');
+        $this->set(compact('articles'));
+    }
+
+}
+
+?>
